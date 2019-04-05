b3986a4a3ac3939ff224e30b2c42175409f578ce
Austin Haag <austinhaag@austins-macbook-pro.local>
2019-04-05 15:11:02 -0500

    Initial Commit

---
+++ Homework5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework5
+//
+//  Created by Austin Haag on 4/5/19.
+//  Copyright © 2019 Austin Haag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
